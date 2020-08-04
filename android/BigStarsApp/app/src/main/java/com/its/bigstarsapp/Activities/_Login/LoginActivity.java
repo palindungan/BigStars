4c6e868657dd50e0f47dcd7e5f0ad8aca0ce201e
palindungan <44224923+palindungan@users.noreply.github.com>
2020-08-04 15:33:49 +0700

    login + main activity
    login + main activity

--- android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/_Login/LoginActivity.java
+++ android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/_Login/LoginActivity.java
@@ -1,14 +1,16 @@
-package com.its.bigstarsapp;
+package com.its.bigstarsapp.Activities._Login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.its.bigstarsapp.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
---
+++ android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/_Main/MainActivity.java
@@ -0,0 +1,16 @@
+package com.its.bigstarsapp.Activities._Main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.its.bigstarsapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
