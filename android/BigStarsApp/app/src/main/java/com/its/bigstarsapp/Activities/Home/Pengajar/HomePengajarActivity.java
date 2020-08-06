33fb9e3a7e0e0723ffc48409f486e116003fcafc
palindungan <44224923+palindungan@users.noreply.github.com>
2020-08-06 13:27:08 +0700

    update
    update

---
+++ android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Home/Pengajar/HomePengajarActivity.java
@@ -0,0 +1,16 @@
+package com.its.bigstarsapp.Activities.Home.Pengajar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.its.bigstarsapp.R;
+
+public class HomePengajarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_pengajar);
+    }
+}
