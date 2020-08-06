f6d91f9dd2700b10a28e67a2a8350e6c7e9392cc
palindungan <44224923+palindungan@users.noreply.github.com>
2020-08-06 09:27:27 +0700

    Update LoginPresenter.java

--- android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/_Login/presenter/LoginPresenter.java
+++ android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/_Login/presenter/LoginPresenter.java
@@ -1,21 +1,120 @@
 package com.its.bigstarsapp.Activities._Login.presenter;
 
 import android.content.Context;
+import android.content.Intent;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.its.bigstarsapp.Activities._Login.view.ILoginView;
+import com.its.bigstarsapp.Controllers.GlobalMessage;
+import com.its.bigstarsapp.Controllers.GlobalProcess;
+import com.its.bigstarsapp.Controllers.GlobalVariable;
+import com.its.bigstarsapp.Controllers.SessionManager;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginPresenter implements ILoginPresenter {
 
     Context context;
     ILoginView loginView;
 
+    GlobalMessage globalMessage;
+    GlobalProcess globalProcess;
+    GlobalVariable globalVariable;
+    SessionManager sessionManager;
+
     public LoginPresenter(Context context, ILoginView loginView) {
         this.context = context;
         this.loginView = loginView;
+
+        globalMessage = new GlobalMessage();
+        globalProcess = new GlobalProcess(context);
+        globalVariable = new GlobalVariable();
+        sessionManager = new SessionManager(context);
     }
 
     @Override
     public void onLogin(String username, String password, String hak_akses) {
+        String base_url = globalVariable.getUrlData();
+        String URL_DATA = base_url + "login/masuk"; // url http request
 
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_DATA,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("success");
+                            String message = jsonObject.getString("message");
+
+                            JSONArray jsonArray = jsonObject.getJSONArray("data_result");
+
+                            if (success.equals("1")) {
+                                for (int i = 0; i < jsonArray.length(); i++) {
+                                    JSONObject object = jsonArray.getJSONObject(i);
+
+                                    String id_user = object.getString("id_user").trim();
+                                    String nama = object.getString("nama").trim();
+                                    String username = object.getString("username").trim();
+
+                                    Intent intent = new Intent();
+
+                                    if (hak_akses.equals("admin")) {
+//                                        intent = new Intent(context, HomeAdminActivity.class);
+                                    } else if (hak_akses.equals("pengajar")) {
+//                                        intent = new Intent(context, HomePengajarActivity.class);
+                                    } else if (hak_akses.equals("wali_murid")) {
+
+                                    } else {
+                                        sessionManager.logout();
+                                    }
+
+                                    sessionManager.setDataUser(
+                                            "" + id_user,
+                                            "" + nama,
+                                            "" + username,
+                                            "" + hak_akses
+                                    );
+
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                    context.startActivity(intent);
+                                }
+                            } else {
+                                globalProcess.onErrorMessage(message);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            globalProcess.onErrorMessage(globalMessage.getMessageResponseError() + e.toString());
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        globalProcess.onErrorMessage(globalMessage.getMessageConnectionError());
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("username", username);
+                params.put("password", password);
+                params.put("hak_akses", hak_akses);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        requestQueue.add(stringRequest);
     }
 }
