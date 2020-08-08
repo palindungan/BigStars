d755c57692451e4bff04eed5856fdecb36cf738e
palindungan <44224923+palindungan@users.noreply.github.com>
2020-08-09 04:19:15 +0700

    Update DataMataPelajaranAddPresenter.java

--- android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Data/MataPelajaran/Add/presenter/DataMataPelajaranAddPresenter.java
+++ android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Data/MataPelajaran/Add/presenter/DataMataPelajaranAddPresenter.java
@@ -2,20 +2,76 @@ package com.its.bigstarsapp.Activities.Data.MataPelajaran.Add.presenter;
 
 import android.content.Context;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.its.bigstarsapp.Activities.Data.MataPelajaran.Add.view.IDataMataPelajaranAddView;
+import com.its.bigstarsapp.Controllers.GlobalMessage;
+import com.its.bigstarsapp.Controllers.GlobalProcess;
+import com.its.bigstarsapp.Controllers.GlobalVariable;
+import com.its.bigstarsapp.Controllers.SessionManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DataMataPelajaranAddPresenter implements IDataMataPelajaranAddPresenter {
 
     Context context;
     IDataMataPelajaranAddView dataMataPelajaranAddView;
 
+    GlobalMessage globalMessage;
+    GlobalProcess globalProcess;
+    GlobalVariable globalVariable;
+    SessionManager sessionManager;
+
     public DataMataPelajaranAddPresenter(Context context, IDataMataPelajaranAddView dataMataPelajaranAddView) {
         this.context = context;
         this.dataMataPelajaranAddView = dataMataPelajaranAddView;
+
+        globalMessage = new GlobalMessage();
+        globalProcess = new GlobalProcess(context);
+        globalVariable = new GlobalVariable();
+        sessionManager = new SessionManager(context);
     }
 
     @Override
     public void onSubmit(String nama) {
+        String base_url = globalVariable.getUrlData();
+        String URL_DATA = base_url + "data/mata_pelajaran/add_data"; // url http request
 
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_DATA,
+                response -> {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        String success = jsonObject.getString("success");
+                        String message = jsonObject.getString("message");
+
+                        if (success.equals("1")) {
+                            globalProcess.onSuccessMessage(message);
+                            dataMataPelajaranAddView.backPressed();
+                        } else {
+                            globalProcess.onErrorMessage(message);
+                        }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        globalProcess.onErrorMessage(globalMessage.getMessageResponseError() + e.toString());
+                    }
+                },
+                error -> globalProcess.onErrorMessage(globalMessage.getMessageConnectionError())) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("nama", nama);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        requestQueue.add(stringRequest);
     }
 }
