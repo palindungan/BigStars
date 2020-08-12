af2f093a39ab038a782fd5d81a86b689a18f9a01
palindungan <44224923+palindungan@users.noreply.github.com>
2020-08-12 15:38:50 +0700

    murid
    murid

---
+++ android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Data/Murid/Edit/DataMuridEditActivity.java
@@ -0,0 +1,16 @@
+package com.its.bigstarsapp.Activities.Data.Murid.Edit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.its.bigstarsapp.R;
+
+public class DataMuridEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_murid_edit);
+    }
+}
---
+++ android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Data/Murid/List/DataMuridListActivity.java
@@ -0,0 +1,16 @@
+package com.its.bigstarsapp.Activities.Data.Murid.List;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.its.bigstarsapp.R;
+
+public class DataMuridListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_murid_list);
+    }
+}
