d85dce4e99cdbd97d1e6549d4764e35c0128c428
palindungan <44224923+palindungan@users.noreply.github.com>
2020-08-12 16:57:41 +0700

    activity
    activity

--- android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Data/Murid/Edit/DataMuridEditActivity.java
+++ android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Data/Murid/Edit/DataMuridEditActivity.java
@@ -8,6 +8,13 @@ import com.its.bigstarsapp.R;
 
 public class DataMuridEditActivity extends AppCompatActivity {
 
+    public static final String EXTRA_ID_MURID = "EXTRA_ID_MURID";
+    public static final String EXTRA_NAMA = "EXTRA_NAMA";
+    public static final String EXTRA_ID_WALI_MURID = "EXTRA_ID_WALI_MURID";
+    public static final String EXTRA_NAMA_WALI_MURID = "EXTRA_NAMA_WALI_MURID";
+    public static final String EXTRA_ALAMAT = "EXTRA_ALAMAT";
+    public static final String EXTRA_FOTO = "EXTRA_FOTO";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Data/Murid/List/DataMuridListActivity.java
+++ android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Data/Murid/List/DataMuridListActivity.java
@@ -1,14 +1,23 @@
 package com.its.bigstarsapp.Activities.Data.Murid.List;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.its.bigstarsapp.Activities.Data.Murid.Add.DataMuridAddActivity;
+import com.its.bigstarsapp.Activities.Data.Murid.Edit.DataMuridEditActivity;
+import com.its.bigstarsapp.Activities.Data.Murid.List.presenter.DataMuridListPresenter;
 import com.its.bigstarsapp.Activities.Data.Murid.List.presenter.IDataMuridListPresenter;
 import com.its.bigstarsapp.Activities.Data.Murid.List.view.IDataMuridListView;
 import com.its.bigstarsapp.Adapters.AdapterDataMuridList;
@@ -30,8 +39,6 @@ public class DataMuridListActivity extends AppCompatActivity implements View.OnC
     GlobalVariable globalVariable;
     SessionManager sessionManager;
 
-    private AdapterDataMuridList adapterDataMuridList;
-
     Toolbar toolbar;
     FloatingActionButton fab;
     private RecyclerView recyclerView;
@@ -43,20 +50,119 @@ public class DataMuridListActivity extends AppCompatActivity implements View.OnC
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_murid_list);
+
+        dataMuridListPresenter = new DataMuridListPresenter(this, this);
+
+        globalMessage = new GlobalMessage();
+        globalProcess = new GlobalProcess(this);
+        globalVariable = new GlobalVariable();
+        sessionManager = new SessionManager(this);
+
+        toolbar = findViewById(R.id.toolbar);
+        fab = findViewById(R.id.fab);
+        recyclerView = findViewById(R.id.recycle_view);
+        swipeRefreshLayout = findViewById(R.id.swipe_refresh_layout);
+
+        globalProcess.initActionBar(toolbar);
+
+        dataMuridListPresenter.onLoadDataList();
+        swipeRefreshLayout.setOnRefreshListener(() -> {
+            // Your code to make your refresh action
+            dataMuridListPresenter.onLoadDataList();
+
+            // CallYourRefreshingMethod();
+            final Handler handler = new Handler();
+            handler.postDelayed(() -> {
+                if (swipeRefreshLayout.isRefreshing()) {
+                    swipeRefreshLayout.setRefreshing(false);
+                }
+            }, 1000);
+        });
+
+        statusActivity = sessionManager.getStatusActivity();
+        if (statusActivity.equals("home->view->editMurid")) {
+            fab.show();
+        }
+
+        fab.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-
+        if (view.getId() == R.id.fab) {
+            startActivity(new Intent(getApplicationContext(), DataMuridAddActivity.class));
+        }
     }
 
     @Override
     public void onSetupListView(ArrayList<Murid> dataModelArrayList) {
+        AdapterDataMuridList adapterDataMuridList = new AdapterDataMuridList(this, dataModelArrayList);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 1, GridLayoutManager.VERTICAL, false);
+        recyclerView.setAdapter(adapterDataMuridList);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setNestedScrollingEnabled(true);
 
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if (dy > 0 && fab.getVisibility() == View.VISIBLE) {
+                    fab.hide();
+                } else if (dy < 0 && fab.getVisibility() != View.VISIBLE) {
+                    if (statusActivity.equals("home->view->editMurid")) {
+                        fab.show();
+                    }
+                }
+            }
+        });
+
+        adapterDataMuridList.setOnItemClickListener((view, position) -> {
+            if (statusActivity.equals("home->view->editMurid")) {
+                Intent intent = new Intent(getApplicationContext(), DataMuridEditActivity.class);
+                intent.putExtra(DataMuridEditActivity.EXTRA_ID_MURID, dataModelArrayList.get(position).getId_murid());
+                intent.putExtra(DataMuridEditActivity.EXTRA_NAMA, dataModelArrayList.get(position).getNama());
+                intent.putExtra(DataMuridEditActivity.EXTRA_ID_WALI_MURID, dataModelArrayList.get(position).getId_wali_murid());
+                intent.putExtra(DataMuridEditActivity.EXTRA_NAMA_WALI_MURID, dataModelArrayList.get(position).getNama_wali_murid());
+                intent.putExtra(DataMuridEditActivity.EXTRA_ALAMAT, dataModelArrayList.get(position).getAlamat());
+                intent.putExtra(DataMuridEditActivity.EXTRA_FOTO, dataModelArrayList.get(position).getFoto());
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
     public void showDialogDelete(String kode, String nama) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                this);
+        alertDialogBuilder.setTitle(globalMessage.getValidasiHapusData() + nama + " ?");
+        alertDialogBuilder
+                .setMessage(globalMessage.getPilihYaHapusData())
+                .setPositiveButton(globalMessage.getYa(), (dialog, id) -> {
 
+                    try {
+                        dataMuridListPresenter.onDelete(kode);
+                    } catch (Exception e) {
+                        globalProcess.onErrorMessage(globalMessage.getErrorHapusData() + e.toString());
+                    }
+
+                })
+                .setNegativeButton(globalMessage.getTidak(), (dialog, id) -> dialog.cancel());
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+        return true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        dataMuridListPresenter.onLoadDataList();
     }
 }
