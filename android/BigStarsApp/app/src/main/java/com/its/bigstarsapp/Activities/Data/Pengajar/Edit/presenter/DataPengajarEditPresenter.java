8156f9a764d28009bc8335fe0db7b331053f6f33
palindungan <44224923+palindungan@users.noreply.github.com>
2020-08-13 18:10:23 +0700

    update done
    update done

--- android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Data/Pengajar/Edit/presenter/DataPengajarEditPresenter.java
+++ android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Data/Pengajar/Edit/presenter/DataPengajarEditPresenter.java
@@ -2,20 +2,81 @@ package com.its.bigstarsapp.Activities.Data.Pengajar.Edit.presenter;
 
 import android.content.Context;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.its.bigstarsapp.Activities.Data.Pengajar.Edit.view.IDataPengajarEditView;
+import com.its.bigstarsapp.Controllers.GlobalMessage;
+import com.its.bigstarsapp.Controllers.GlobalProcess;
+import com.its.bigstarsapp.Controllers.GlobalVariable;
+import com.its.bigstarsapp.Controllers.SessionManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DataPengajarEditPresenter implements IDataPengajarEditPresenter {
 
     Context context;
     IDataPengajarEditView dataPengajarEditView;
 
+    GlobalMessage globalMessage;
+    GlobalProcess globalProcess;
+    GlobalVariable globalVariable;
+    SessionManager sessionManager;
+
     public DataPengajarEditPresenter(Context context, IDataPengajarEditView dataPengajarEditView) {
         this.context = context;
         this.dataPengajarEditView = dataPengajarEditView;
+
+        globalMessage = new GlobalMessage();
+        globalProcess = new GlobalProcess(context);
+        globalVariable = new GlobalVariable();
+        sessionManager = new SessionManager(context);
     }
 
     @Override
     public void onUpdate(String id_pengajar, String nama, String username, String password, String alamat, String no_hp, String foto) {
+        String base_url = globalVariable.getUrlData();
+        String URL_DATA = base_url + "data/pengajar/update_data"; // url http request
 
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_DATA,
+                response -> {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        String success = jsonObject.getString("success");
+                        String message = jsonObject.getString("message");
+
+                        if (success.equals("1")) {
+                            globalProcess.onSuccessMessage(message);
+                            dataPengajarEditView.backPressed();
+                        } else {
+                            globalProcess.onErrorMessage(message);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        globalProcess.onErrorMessage(globalMessage.getMessageResponseError() + e.toString());
+                    }
+                },
+                error -> globalProcess.onErrorMessage(globalMessage.getMessageConnectionError())) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("id_pengajar", id_pengajar);
+                params.put("nama", nama);
+                params.put("username", username);
+                params.put("password", password);
+                params.put("alamat", alamat);
+                params.put("no_hp", no_hp);
+                params.put("foto", foto);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        requestQueue.add(stringRequest);
     }
 }
