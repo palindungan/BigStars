3715b64d4514929647bae8c3c8bc29ef6735d8c1
palindungan <44224923+palindungan@users.noreply.github.com>
2020-08-13 08:23:34 +0700

    add
    add

--- android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Home/Admin/HomeAdminActivity.java
+++ android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Home/Admin/HomeAdminActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import com.google.android.material.navigation.NavigationView;
 import com.its.bigstarsapp.Activities.Akun.Admin.AkunAdminActivity;
 import com.its.bigstarsapp.Activities.Data.MataPelajaran.List.DataMataPelajaranListActivity;
+import com.its.bigstarsapp.Activities.Data.Murid.List.DataMuridListActivity;
 import com.its.bigstarsapp.Activities.Data.WaliMurid.List.DataWaliMuridListActivity;
 import com.its.bigstarsapp.Controllers.GlobalProcess;
 import com.its.bigstarsapp.Controllers.SessionManager;
@@ -99,9 +100,9 @@ public class HomeAdminActivity extends AppCompatActivity implements View.OnClick
 //            intent = new Intent(getApplicationContext(), DataPengajarListActivity.class);
 //            startActivity(intent);
         } else if (view.getId() == R.id.link_admin_murid) {
-//            sessionManager.setStatusActivity("home->view->editMurid");
-//            intent = new Intent(getApplicationContext(), DataMuridListActivity.class);
-//            startActivity(intent);
+            sessionManager.setStatusActivity("home->view->editMurid");
+            intent = new Intent(getApplicationContext(), DataMuridListActivity.class);
+            startActivity(intent);
         } else if (view.getId() == R.id.link_admin_wali_murid) {
             sessionManager.setStatusActivity("home->view->editWaliMurid");
             intent = new Intent(getApplicationContext(), DataWaliMuridListActivity.class);
--- web/application/models/api/M_kode.php
+++ web/application/models/api/M_kode.php
@@ -1,6 +1,27 @@
 <?php
 class M_kode extends CI_Model
 {
+
+    function id_murid()
+    {
+        $field = "id_murid";
+        $tabel = "murid";
+        $digit = "5";
+        $kode = "MR";
+
+        $q = $this->db->query("SELECT MAX(RIGHT($field,$digit)) AS kd_max FROM $tabel");
+        $kd = "";
+        if ($q->num_rows() > 0) {
+            foreach ($q->result() as $k) {
+                $tmp = ((int) $k->kd_max) + 1;
+                $kd = $kode . sprintf('%0' . $digit . 's',  $tmp);
+            }
+        } else {
+            $kd = "MR00001";
+        }
+        return $kd;
+    }
+
     // // autogenerate kode / ID
     // function get_id_pengajar()
     // {
@@ -18,26 +39,6 @@ class M_kode extends CI_Model
     //         }
     //     } else {
     //         $kd = "PE001";
-    //     }
-    //     return $kd;
-    // }
-
-    // function get_id_murid()
-    // {
-    //     $field = "id_murid";
-    //     $tabel = "murid";
-    //     $digit = "3";
-    //     $kode = "MR";
-
-    //     $q = $this->db->query("SELECT MAX(RIGHT($field,$digit)) AS kd_max FROM $tabel");
-    //     $kd = "";
-    //     if ($q->num_rows() > 0) {
-    //         foreach ($q->result() as $k) {
-    //             $tmp = ((int) $k->kd_max) + 1;
-    //             $kd = $kode . sprintf('%0' . $digit . 's',  $tmp);
-    //         }
-    //     } else {
-    //         $kd = "MR001";
     //     }
     //     return $kd;
     // }
