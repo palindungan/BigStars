b56ea34e64b77fa30818933f59b2a2b3178230ac
palindungan <44224923+palindungan@users.noreply.github.com>
2020-08-16 16:56:01 +0700

    Update DataKelasPertemuanAddPresenter.java

--- android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Data/KelasPertemuan/Add/presenter/DataKelasPertemuanAddPresenter.java
+++ android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Data/KelasPertemuan/Add/presenter/DataKelasPertemuanAddPresenter.java
@@ -2,24 +2,133 @@ package com.its.bigstarsapp.Activities.Data.KelasPertemuan.Add.presenter;
 
 import android.content.Context;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.its.bigstarsapp.Activities.Data.KelasPertemuan.Add.view.IDataKelasPertemuanAddView;
+import com.its.bigstarsapp.Controllers.GlobalMessage;
+import com.its.bigstarsapp.Controllers.GlobalProcess;
+import com.its.bigstarsapp.Controllers.GlobalVariable;
+import com.its.bigstarsapp.Controllers.SessionManager;
+import com.its.bigstarsapp.Models.MataPelajaran;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DataKelasPertemuanAddPresenter implements IDataKelasPertemuanAddPresenter {
     Context context;
     IDataKelasPertemuanAddView dataKelasPertemuanAddView;
 
+    GlobalMessage globalMessage;
+    GlobalProcess globalProcess;
+    GlobalVariable globalVariable;
+    SessionManager sessionManager;
+
+    ArrayList<MataPelajaran> dataModelArrayList;
+
     public DataKelasPertemuanAddPresenter(Context context, IDataKelasPertemuanAddView dataKelasPertemuanAddView) {
         this.context = context;
         this.dataKelasPertemuanAddView = dataKelasPertemuanAddView;
+
+        globalMessage = new GlobalMessage();
+        globalProcess = new GlobalProcess(context);
+        globalVariable = new GlobalVariable();
+        sessionManager = new SessionManager(context);
     }
 
     @Override
     public void onSubmit(String id_pengajar, String id_mata_pelajaran, String hari, String jam_mulai, String jam_berakhir, String harga_fee, String harga_spp) {
+        String base_url = globalVariable.getUrlData();
+        String URL_DATA = base_url + "data/kelas_pertemuan/add_data"; // url http request
 
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_DATA,
+                response -> {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        String success = jsonObject.getString("success");
+                        String message = jsonObject.getString("message");
+
+                        if (success.equals("1")) {
+                            globalProcess.onSuccessMessage(message);
+                            dataKelasPertemuanAddView.backPressed();
+                        } else {
+                            globalProcess.onErrorMessage(message);
+                        }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        globalProcess.onErrorMessage(globalMessage.getMessageResponseError() + e.toString());
+                    }
+                },
+                error -> globalProcess.onErrorMessage(globalMessage.getMessageConnectionError())) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("id_pengajar", id_pengajar);
+                params.put("id_mata_pelajaran", id_mata_pelajaran);
+                params.put("hari", hari);
+                params.put("jam_mulai", jam_mulai);
+                params.put("jam_berakhir", jam_berakhir);
+                params.put("harga_fee", harga_fee);
+                params.put("harga_spp", harga_spp);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        requestQueue.add(stringRequest);
     }
 
     @Override
     public void onLoadDataListMataPelajaran() {
+        String base_url = globalVariable.getUrlData();
+        String URL_DATA = base_url + "data/mata_pelajaran/list_data"; // url http request
 
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL_DATA, response -> {
+            try {
+
+                JSONObject obj = new JSONObject(response);
+
+                String success = obj.getString("success");
+                String message = obj.getString("message");
+
+                if (success.equals("1")) {
+
+                    dataModelArrayList = new ArrayList<>();
+                    JSONArray dataArray = obj.getJSONArray("data_result");
+                    for (int i = 0; i < dataArray.length(); i++) {
+
+                        MataPelajaran playerModel = new MataPelajaran();
+                        JSONObject dataobj = dataArray.getJSONObject(i);
+
+                        String id_mata_pelajaran = dataobj.getString("id_mata_pelajaran");
+                        String nama = dataobj.getString("nama");
+
+                        playerModel.setId_mata_pelajaran(id_mata_pelajaran);
+                        playerModel.setNama(nama);
+
+                        dataModelArrayList.add(playerModel);
+                    }
+                    dataKelasPertemuanAddView.onSetupListView(dataModelArrayList);
+
+                } else {
+                    dataModelArrayList = new ArrayList<>();
+                    dataKelasPertemuanAddView.onSetupListView(dataModelArrayList);
+                    globalProcess.onErrorMessage(message);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                globalProcess.onErrorMessage(globalMessage.getMessageResponseError() + e.toString());
+            }
+        }, error -> globalProcess.onErrorMessage(globalMessage.getMessageConnectionError()));
+
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        requestQueue.add(stringRequest);
     }
 }
