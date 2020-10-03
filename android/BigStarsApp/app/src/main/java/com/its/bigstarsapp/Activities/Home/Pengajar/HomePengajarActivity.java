7e4902a5bfdf1bda50c4e49eaa1afff971b067e3
palindungan <44224923+palindungan@users.noreply.github.com>
2020-10-03 13:50:04 +0700

    home logic
    home logic

--- android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Home/Pengajar/HomePengajarActivity.java
+++ android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Home/Pengajar/HomePengajarActivity.java
@@ -1,16 +1,51 @@
 package com.its.bigstarsapp.Activities.Home.Pengajar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.its.bigstarsapp.Controllers.GlobalProcess;
+import com.its.bigstarsapp.Controllers.SessionManager;
 import com.its.bigstarsapp.R;
 
-public class HomePengajarActivity extends AppCompatActivity {
+public class HomePengajarActivity extends AppCompatActivity implements View.OnClickListener {
+
+    SessionManager sessionManager;
+    GlobalProcess globalProcess;
+
+    CardView linkPengajarKelasPertemuan, linkPengajarKelasPertemuanAktif, linkPengajarRiwayatAbsen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_pengajar);
+
+        sessionManager = new SessionManager(this);
+        globalProcess = new GlobalProcess(this);
+
+        linkPengajarKelasPertemuan = findViewById(R.id.link_pengajar_kelas_pertemuan);
+        linkPengajarKelasPertemuanAktif = findViewById(R.id.link_pengajar_kelas_pertemuan_aktif);
+        linkPengajarRiwayatAbsen = findViewById(R.id.link_pengajar_riwayat_absen);
+
+        linkPengajarKelasPertemuan.setOnClickListener(this);
+        linkPengajarKelasPertemuanAktif.setOnClickListener(this);
+        linkPengajarRiwayatAbsen.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        if (view.getId() == R.id.link_pengajar_kelas_pertemuan) {
+//            sessionManager.setStatusActivity("home->view->editPengajar");
+//            intent = new Intent(getApplicationContext(), DataPengajarListActivity.class);
+//            startActivity(intent);
+        } else if (view.getId() == R.id.link_pengajar_kelas_pertemuan_aktif) {
+            //
+        } else if (view.getId() == R.id.link_pengajar_riwayat_absen) {
+            //
+        }
     }
 }
