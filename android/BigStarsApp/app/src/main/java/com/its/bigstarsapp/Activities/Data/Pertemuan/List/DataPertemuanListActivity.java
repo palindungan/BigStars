ba3ebeb6cc603605080d0ddee956b494796536c7
palindungan <44224923+palindungan@users.noreply.github.com>
2020-10-04 22:08:10 +0700

    logic
    logic

--- android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Data/Pertemuan/List/DataPertemuanListActivity.java
+++ android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Data/Pertemuan/List/DataPertemuanListActivity.java
@@ -1,11 +1,24 @@
 package com.its.bigstarsapp.Activities.Data.Pertemuan.List;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.its.bigstarsapp.Activities.Data.Pertemuan.List.presenter.DataPertemuanListPresenter;
+import com.its.bigstarsapp.Activities.Data.Pertemuan.List.presenter.IDataPertemuanListPresenter;
 import com.its.bigstarsapp.Activities.Data.Pertemuan.List.view.IDataPertemuanListView;
+import com.its.bigstarsapp.Adapters.AdapterDataPertemuanList;
+import com.its.bigstarsapp.Controllers.GlobalMessage;
+import com.its.bigstarsapp.Controllers.GlobalProcess;
+import com.its.bigstarsapp.Controllers.GlobalVariable;
+import com.its.bigstarsapp.Controllers.SessionManager;
 import com.its.bigstarsapp.Models.Pertemuan;
 import com.its.bigstarsapp.R;
 
@@ -13,10 +26,59 @@ import java.util.ArrayList;
 
 public class DataPertemuanListActivity extends AppCompatActivity implements View.OnClickListener, IDataPertemuanListView {
 
+    public static final String EXTRA_ID_PENGAJAR = "EXTRA_ID_PENGAJAR";
+
+    IDataPertemuanListPresenter dataPertemuanListPresenter;
+
+    GlobalMessage globalMessage;
+    GlobalProcess globalProcess;
+    GlobalVariable globalVariable;
+    SessionManager sessionManager;
+
+    Toolbar toolbar;
+    private RecyclerView recyclerView;
+    private SwipeRefreshLayout swipeRefreshLayout;
+
+    String id_pengajar;
+
+    String statusActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_pertemuan_list);
+
+        dataPertemuanListPresenter = new DataPertemuanListPresenter(this, this);
+
+        globalMessage = new GlobalMessage();
+        globalProcess = new GlobalProcess(this);
+        globalVariable = new GlobalVariable();
+        sessionManager = new SessionManager(this);
+
+        toolbar = findViewById(R.id.toolbar);
+        recyclerView = findViewById(R.id.recycle_view);
+        swipeRefreshLayout = findViewById(R.id.swipe_refresh_layout);
+
+        globalProcess.initActionBar(toolbar);
+
+        id_pengajar = getIntent().getStringExtra(EXTRA_ID_PENGAJAR);
+
+        statusActivity = sessionManager.getStatusActivity();
+
+        dataPertemuanListPresenter.onLoadDataList("" + id_pengajar);
+
+        swipeRefreshLayout.setOnRefreshListener(() -> {
+            // Your code to make your refresh action
+            dataPertemuanListPresenter.onLoadDataList("" + id_pengajar);
+
+            // CallYourRefreshingMethod();
+            final Handler handler = new Handler();
+            handler.postDelayed(() -> {
+                if (swipeRefreshLayout.isRefreshing()) {
+                    swipeRefreshLayout.setRefreshing(false);
+                }
+            }, 1000);
+        });
     }
 
     @Override
@@ -26,6 +88,32 @@ public class DataPertemuanListActivity extends AppCompatActivity implements View
 
     @Override
     public void onSetupListView(ArrayList<Pertemuan> dataModelArrayList) {
+        AdapterDataPertemuanList adapterDataPertemuanList = new AdapterDataPertemuanList(this, dataModelArrayList);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 1, GridLayoutManager.VERTICAL, false);
+        recyclerView.setAdapter(adapterDataPertemuanList);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setNestedScrollingEnabled(true);
+        adapterDataPertemuanList.notifyDataSetChanged();
 
+//        adapterDataPertemuanList.setOnItemClickListener((view, position) -> {
+//            Intent intent;
+//            if (statusActivity.equals("")) {
+//
+//            }
+//        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+        return true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        dataPertemuanListPresenter.onLoadDataList("" + id_pengajar);
     }
 }
--- android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Adapters/AdapterDataPertemuanList.java
+++ android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Adapters/AdapterDataPertemuanList.java
@@ -1,12 +1,25 @@
 package com.its.bigstarsapp.Adapters;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.its.bigstarsapp.Models.Pertemuan;
+
+import java.util.ArrayList;
+
 public class AdapterDataPertemuanList extends RecyclerView.Adapter<AdapterDataPertemuanList.AdapterDataPertemuanListViewHolder> {
+    Context context;
+    ArrayList<Pertemuan> dataModelArrayList;
+
+    public AdapterDataPertemuanList(Context context, ArrayList<Pertemuan> dataModelArrayList) {
+        this.context = context;
+        this.dataModelArrayList = dataModelArrayList;
+    }
+
     @NonNull
     @Override
     public AdapterDataPertemuanListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
