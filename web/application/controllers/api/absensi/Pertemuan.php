ab0bd2faaa259dfa7ccd27a04b90e16edc3e7856
palindungan <44224923+palindungan@users.noreply.github.com>
2020-10-05 20:22:17 +0700

    Update Pertemuan.php

--- web/application/controllers/api/absensi/Pertemuan.php
+++ web/application/controllers/api/absensi/Pertemuan.php
@@ -118,4 +118,62 @@ class Pertemuan extends REST_Controller
             $this->response($result, 200);
         }
     }
+
+    function list_data_post()
+    {
+        $id_pengajar        = $this->post('id_pengajar');
+
+        // variable array
+        $result['data_result'] = array();
+
+        // data array untuk where db
+        $where = array(
+            'id_pengajar' => $id_pengajar,
+            'status_pertemuan' => 'Belum Selesai'
+        );
+
+        // mengambil data
+        $query = $this->M_universal->get_data('view_pertemuan', $where);
+
+        if ($query->num_rows() > 0) {
+
+            // mengeluarkan data dari database
+            foreach ($query->result_array() as $row) {
+
+                // ambil detail data db
+                $data = array(
+                    'id_pertemuan'          => $row["id_pertemuan"],
+                    'id_pengajar'           => $row["id_pengajar"],
+                    'id_kelas_pertemuan'    => $row["id_kelas_pertemuan"],
+
+                    'hari'                  => $row["hari"],
+                    'waktu_mulai'           => $row["waktu_mulai"],
+                    'waktu_berakhir'        => $row["waktu_berakhir"],
+                    'lokasi_mulai_la'       => $row["lokasi_mulai_la"],
+                    'lokasi_mulai_lo'       => $row["lokasi_mulai_lo"],
+                    'lokasi_berakhir_la'    => $row["lokasi_berakhir_la"],
+                    'lokasi_berakhir_lo'    => $row["lokasi_berakhir_lo"],
+                    'deskripsi'             => $row["deskripsi"],
+                    'harga_fee'             => $row["harga_fee"],
+                    'harga_spp'             => $row["harga_spp"],
+                    'status_fee'            => $row["status_fee"],
+                    'status_spp'            => $row["status_spp"],
+                    'status_konfirmasi'     => $row["status_konfirmasi"],
+                    'status_pertemuan'      => $row["status_pertemuan"],
+                );
+
+                array_push($result['data_result'], $data);
+
+                // membuat array untuk di transfer
+                $result["success"] = "1";
+                $result["message"] = "Success Berhasil Mengambil Data";
+                $this->response($result, 200);
+            }
+        } else {
+            // membuat array untuk di transfer ke API
+            $result["success"] = "0";
+            $result["message"] = "Data Masih kosong";
+            $this->response($result, 200);
+        }
+    }
 }
