bca2e64fedce33a0cb1a6d1495b8f571ac34adfa
palindungan <rizkika.palindungan1@gmail.com>
2020-10-31 21:44:55 +0700

    design
    design

--- android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Data/Pembayaran/Fee/Detail/DataPembayaranFeeDetailActivity.java
+++ android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Data/Pembayaran/Fee/Detail/DataPembayaranFeeDetailActivity.java
@@ -1,10 +1,13 @@
 package com.its.bigstarsapp.Activities.Data.Pembayaran.Fee.Detail;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 
+import com.its.bigstarsapp.Activities.Data.Pembayaran.Fee.Detail.presenter.DataPembayaranFeeDetailPresenter;
 import com.its.bigstarsapp.Activities.Data.Pembayaran.Fee.Detail.presenter.IDataPembayaranFeeDetailPresenter;
 import com.its.bigstarsapp.Activities.Data.Pembayaran.Fee.Detail.view.IDataPembayaranFeeDetailView;
 import com.its.bigstarsapp.Controllers.GlobalMessage;
@@ -22,10 +25,25 @@ public class DataPembayaranFeeDetailActivity extends AppCompatActivity implement
     GlobalVariable globalVariable;
     SessionManager sessionManager;
 
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_pembayaran_fee_detail);
+
+        dataPembayaranFeeDetailPresenter = new DataPembayaranFeeDetailPresenter(this, this);
+
+        globalMessage = new GlobalMessage();
+        globalProcess = new GlobalProcess(this);
+        globalVariable = new GlobalVariable();
+        sessionManager = new SessionManager(this);
+
+        toolbar = findViewById(R.id.toolbar);
+        recyclerView = findViewById(R.id.recycle_view);
+
+        globalProcess.initActionBar(toolbar);
     }
 
     @Override
--- android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Data/Pembayaran/Fee/Detail/presenter/DataPembayaranFeeDetailPresenter.java
+++ android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Data/Pembayaran/Fee/Detail/presenter/DataPembayaranFeeDetailPresenter.java
@@ -1,4 +1,29 @@
 package com.its.bigstarsapp.Activities.Data.Pembayaran.Fee.Detail.presenter;
 
+import android.content.Context;
+
+import com.its.bigstarsapp.Activities.Data.Pembayaran.Fee.Detail.view.IDataPembayaranFeeDetailView;
+import com.its.bigstarsapp.Controllers.GlobalMessage;
+import com.its.bigstarsapp.Controllers.GlobalProcess;
+import com.its.bigstarsapp.Controllers.GlobalVariable;
+import com.its.bigstarsapp.Controllers.SessionManager;
+
 public class DataPembayaranFeeDetailPresenter implements IDataPembayaranFeeDetailPresenter {
+    Context context;
+    IDataPembayaranFeeDetailView dataPembayaranFeeDetailView;
+
+    GlobalMessage globalMessage;
+    GlobalProcess globalProcess;
+    GlobalVariable globalVariable;
+    SessionManager sessionManager;
+
+    public DataPembayaranFeeDetailPresenter(Context context, IDataPembayaranFeeDetailView dataPembayaranFeeDetailView) {
+        this.context = context;
+        this.dataPembayaranFeeDetailView = dataPembayaranFeeDetailView;
+
+        globalMessage = new GlobalMessage();
+        globalProcess = new GlobalProcess(context);
+        globalVariable = new GlobalVariable();
+        sessionManager = new SessionManager(context);
+    }
 }
