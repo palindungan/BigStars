731bc9143d52eb7d53026a58357b10e0dcb95cc8
palindungan <rizkika.palindungan1@gmail.com>
2020-11-07 22:24:26 +0700

    Update DataPembayaranSppDetailActivity.java

--- android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Data/Pembayaran/Spp/Detail/DataPembayaranSppDetailActivity.java
+++ android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Data/Pembayaran/Spp/Detail/DataPembayaranSppDetailActivity.java
@@ -1,17 +1,47 @@
 package com.its.bigstarsapp.Activities.Data.Pembayaran.Spp.Detail;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.its.bigstarsapp.Activities.Data.Pembayaran.Spp.Detail.presenter.IDataPembayaranSppDetailPresenter;
 import com.its.bigstarsapp.Activities.Data.Pembayaran.Spp.Detail.view.IDataPembayaranSppDetailView;
+import com.its.bigstarsapp.Controllers.GlobalMessage;
+import com.its.bigstarsapp.Controllers.GlobalProcess;
+import com.its.bigstarsapp.Controllers.GlobalVariable;
+import com.its.bigstarsapp.Controllers.SessionManager;
 import com.its.bigstarsapp.Models.Pertemuan;
 import com.its.bigstarsapp.R;
 
 import java.util.ArrayList;
 
 public class DataPembayaranSppDetailActivity extends AppCompatActivity implements View.OnClickListener, IDataPembayaranSppDetailView {
+
+    public static final String EXTRA_ID_BAYAR_SPP = "EXTRA_ID_BAYAR_SPP";
+    public static final String EXTRA_ID_WALI_MURID = "EXTRA_ID_WALI_MURID";
+
+    IDataPembayaranSppDetailPresenter dataPembayaranSppDetailPresenter;
+
+    GlobalMessage globalMessage;
+    GlobalProcess globalProcess;
+    GlobalVariable globalVariable;
+    SessionManager sessionManager;
+
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    EditText edtNamaWaliMurid, edtTotalPertemuan, edtTotalSpp;
+    Button btnBayar;
+
+    String id_wali_murid, id_bayar_spp, id_user;
+    String hak_akses;
+    String statusActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
