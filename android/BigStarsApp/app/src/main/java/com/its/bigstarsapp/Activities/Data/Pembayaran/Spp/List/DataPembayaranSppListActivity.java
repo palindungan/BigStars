dd47bfc4e18009e25e200c2f873b6b3e17b81313
palindungan <rizkika.palindungan1@gmail.com>
2020-11-08 14:44:26 +0700

    view
    view

--- android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Data/Pembayaran/Spp/List/DataPembayaranSppListActivity.java
+++ android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Data/Pembayaran/Spp/List/DataPembayaranSppListActivity.java
@@ -3,14 +3,21 @@ package com.its.bigstarsapp.Activities.Data.Pembayaran.Spp.List;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.its.bigstarsapp.Activities.Data.Pembayaran.Spp.List.view.IDataPembayaranSppListView;
 import com.its.bigstarsapp.R;
 
-public class DataPembayaranSppListActivity extends AppCompatActivity {
+public class DataPembayaranSppListActivity extends AppCompatActivity implements View.OnClickListener, IDataPembayaranSppListView {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_pembayaran_spp_list);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
