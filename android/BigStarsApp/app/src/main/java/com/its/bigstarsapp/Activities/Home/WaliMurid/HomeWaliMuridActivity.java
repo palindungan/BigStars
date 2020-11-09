678acf5dfe2bf3e4319cff33b0f191e3bfb7c9cf
palindungan <rizkika.palindungan1@gmail.com>
2020-11-09 10:05:25 +0700

    init home wali murid
    init home wali murid

---
+++ android/BigStarsApp/app/src/main/java/com/its/bigstarsapp/Activities/Home/WaliMurid/HomeWaliMuridActivity.java
@@ -0,0 +1,16 @@
+package com.its.bigstarsapp.Activities.Home.WaliMurid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.its.bigstarsapp.R;
+
+public class HomeWaliMuridActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_wali_murid);
+    }
+}
